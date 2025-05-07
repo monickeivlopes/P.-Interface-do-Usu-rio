fbdd15ad8bcd77d56b62588909bfc5eb553d082c
monicke <monickelara@icloud.com>
2025-05-07 14:25:16 -0300

    initial commit

---
+++ aulaUm/src/MyFirstComponent.jsx
@@ -0,0 +1,20 @@
+function MyFirstComponent () {
+
+    const nome = "monicke"
+
+    return (
+        <>
+
+        <div>
+            <h1>Olá mundo</h1>
+        </div>
+
+        <div>
+            <p>Seu nome é {nome}</p>
+        </div>
+
+        </>
+    );
+}
+
+export default MyFirstComponent
