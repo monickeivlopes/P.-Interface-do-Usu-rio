035995821c0ca3f595736a631280e9335db6face
monicke <monickelara@icloud.com>
2025-07-02 14:22:37 -0300

    commit

--- Tarefas/src/tasks.js
+++ Tarefas/src/tasks.js
@@ -2,4 +2,6 @@ export const tasks = [
   { id: 1, title: "Estudar React", status: "pendente" },
   { id: 2, title: "Ler um livro", status: "realizada" },
   { id: 3, title: "Organizar a mesa", status: "nao_realizada" },
+  { id: 4, title: "Terminar atividade", status: "nao_realizada" },
+  { id: 5, title: "Imprimir documentos", status: "nao_realizada" },
 ];
